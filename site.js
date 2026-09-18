c7e93483013af8f1a550b8933cb3fd76efb5cca5
Charles85298 <charles.g.fleming@gmail.com>
2026-09-18 02:42:16 -0700

    Add files via upload

---
+++ site.js
@@ -0,0 +1,9 @@
+
+const btn=document.querySelector('.menu-btn'), links=document.querySelector('.links');
+if(btn&&links) btn.addEventListener('click',()=>links.classList.toggle('open'));
+document.querySelectorAll('.links a').forEach(a=>a.addEventListener('click',()=>links?.classList.remove('open')));
+
+  e.preventDefault();
+  const msg=f.querySelector('.form-msg');
+  if(msg) msg.textContent='Thank you. Your request has been captured for this website demo.';
+}));
